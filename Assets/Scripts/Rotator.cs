6492ee8fdd6fc4cd84bc5d89a70b9988d90161a7
AlexanderRN <AlexanderRNielsen@hotmail.dk>
2016-02-16 11:00:08 +0100

    Roll A Ball V2 Git
    GitHub push of Roll A Boll.

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class Rotator : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+
+		//Vector3 destination = new Vector3 (0, transform.position.y, 5);
+		//transform.DOMove (destination, 2f);
+
+	}
+}
